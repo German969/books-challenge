219d9081f09a832e2335ea68d19c4ca856e8cd74
German Monzon <german.monzon.95@gmail.com>
2022-02-09 19:58:24 -0300

    add authors view

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,16 +1,16 @@
-const db = require("../database/models");
+const db = require('../database/models');
 
 const mainController = {
   home: (req, res) => {
     db.Book.findAll()
-    .then(books => {
-    
-      res.send(books)
-      /* res.render('home', books); */
-    })
-    .catch(error => console.log(error))
-   
-    
+      .then((books) => {
+        res.send(books);
+        /* res.render('home', books); */
+      })
+      .catch((error) => console.log(error));
+  },
+  authors: (req, res) => {
+    res.render('authors');
   }
 };
 
--- src/routes/main.js
+++ src/routes/main.js
@@ -4,5 +4,6 @@ const mainController = require('../controllers/main');
 const router = express.Router();
 
 router.get('/', mainController.home);
+router.get('/authors', mainController.authors);
 
 module.exports = router;
